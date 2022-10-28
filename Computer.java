f1f49397a78a71458aba2750450d39c917d5c8a4
NishanthiKumar <nishanthikumar260502@gmail.com>
2022-10-28 13:21:25 +0530

    ExPro

---
+++ Computer.java
@@ -0,0 +1,10 @@
+package lms;
+
+public class Computer {
+	String brand;
+	float displaySize;
+	int price;
+	int taxPercentage;
+	int netPrice;
+
+}
---
+++ UseComputer.java
@@ -0,0 +1,15 @@
+package lms;
+
+public class UseComputer {
+	public static void main(String[] args) {
+		String[] values=args[0].split(",");
+		Computer computer=new Computer();
+		computer.brand=values[0];
+		computer.displaySize=Float.parseFloat(values[1]);
+		computer.price=Integer.parseInt(values[2]);
+		computer.taxPercentage=Integer.parseInt(values[3]);
+		computer.netPrice=computer.price+(computer.price*computer.taxPercentage/100);
+		System.out.println("Brand="+computer.brand+" DisplaySize="+computer.displaySize+" Price="+computer.price+" Netprice="+computer.netPrice);
+	}
+
+}
